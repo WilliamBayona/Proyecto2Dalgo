0b7565a51603750121b9d1220b4d30a539f43071
DanielaE1512 <d.echavarria@uniandes.edu.co>
2024-11-10 14:08:47 -0500

    CelulaBloqueada

--- ProblemaP2.java
+++ ProblemaP2.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -7,11 +10,15 @@ import java.util.Queue;
 public class ProblemaP2 {
 
     private int n; // Número de nodos
-    private Map<Integer, Map<Integer, Integer>> graph;
+    private Map<Integer, Map<Integer, Integer>> originalGrafo;
+    private Map<Integer, Map<Integer, Integer>> grafo;
+    private ArrayList< Integer> calculadoras;
     
 
     public static void main(String[] args) {
-        int n = 6; // Número de nodos en la red
+        ProblemaP2 problema = new ProblemaP2();
+        problema.n = 6; 
+        problema.grafo = problema.originalGrafo;
       
     }
 
@@ -24,7 +31,7 @@ private boolean bfs(int source, int sink, int[] parent) {
         while (!queue.isEmpty()) {
             int u = queue.poll();
 
-            for (Map.Entry<Integer, Integer> entry : this.graph.get(u).entrySet()) {
+            for (Map.Entry<Integer, Integer> entry : this.grafo.get(u).entrySet()) {
                 int v = entry.getKey();
                 int capacity = entry.getValue();
 
@@ -46,24 +53,60 @@ private boolean bfs(int source, int sink, int[] parent) {
         int maxFlow = 0;
 
         while (this.bfs(source, sink, parent)) {
-            // Encontrar el flujo máximo a través del camino encontrado
+            // Encontrar el flujo máximo a través del camino encontrado cuello de botella
             int pathFlow = Integer.MAX_VALUE;
             for (int v = sink; v != source; v = parent[v]) {
                 int u = parent[v];
-                pathFlow = Math.min(pathFlow, this.graph.get(u).get(v));
+                pathFlow = Math.min(pathFlow, this.grafo.get(u).get(v));
             }
 
             // Actualizar las capacidades de las aristas y las aristas inversas
             for (int v = sink; v != source; v = parent[v]) {
                 int u = parent[v];
-                this.graph.get(u).put(v, this.graph.get(u).get(v) - pathFlow);
-                this.graph.get(v).put(u, this.graph.get(v).getOrDefault(u, 0) + pathFlow);
+                this.grafo.get(u).put(v, this.grafo.get(u).get(v) - pathFlow);
+                this.grafo.get(v).put(u, this.grafo.get(v).getOrDefault(u, 0) + pathFlow);
             }
 
             maxFlow += pathFlow;
         }
 
         return maxFlow;
+    }
+
+
+      public Map<Integer, Map<Integer, Integer>> blockNode(int node) {
+        
+        this.grafo = originalGrafo;
+
+        for (int i = 0; i < this.n; i++) {
+            if (this.originalGrafo.get(i).containsKey(node)) {
+                this.grafo.get(i).put(node, 0);
+            }
+            if (this.originalGrafo.get(node).containsKey(i)) {
+                this.grafo.get(node).put(i, 0);
+            }
+        }
+        return grafo;
+    }
+
+    //Funcion a llamar para encontrar el nodo a bloquear
+    public int[] findBestNodeToBlock(int source, int sink, List<Integer> calculatorNodes) {
+        //Encontrar flujo original
+        int originalMaxFlow = this.edmondsKarp(source, sink);
+        int minimoFlujo = originalMaxFlow;
+        int bestNode = -1;
+
+        for (int node : calculadoras) {
+            grafo = this.blockNode(node);
+            int newMaxFlow = this.edmondsKarp(source, sink);
+
+            if (minimoFlujo > newMaxFlow ) {
+                minimoFlujo = newMaxFlow;
+                bestNode = node;
+            }
+        }
+
+        return new int[]{bestNode,originalMaxFlow,minimoFlujo};
     }
 
    
